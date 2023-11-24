117bf37cc00d02460d56b4024f1ce808c575acf0
hajun <audgkwns44@gmail.com>
2023-11-24 16:52:16 +0900

    [Create]Default Buttons, few functions

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,16 +1,35 @@
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function HomePage() {
   const [lineData, setLineData] = useState([
-    <Typography>Welcome To HPBD-KDJ v1.0</Typography>,
-    <Typography>이 시스템은 KDJ의 생일을</Typography>,
-    <Typography>축하하기 위해 만들어졌습니다</Typography>,
-    <Typography />,
-    <Typography>help를 입력해 도움을 얻으세요</Typography>,
+    <Typography className="code">Welcome To HPBD-KDJ v1.0</Typography>,
+    <Typography className="code">이 시스템은 KDJ의 생일을</Typography>,
+    <Typography className="code">축하하기 위해 만들어졌습니다</Typography>,
+    <Typography className="code" />,
+    <Typography className="code">help를 입력해 도움을 얻으세요</Typography>,
   ]);
+
+  const [choices, setChoices] = useState(["help"]);
+
+  const newLineWriter = useCallback((content) => {
+    if (content === "clear") {
+      setLineData([]);
+    } else if (content === "help") {
+      setChoices(["help", "login", "get_cele", "clear"]);
+      setLineData((prev) => [
+        ...prev,
+        <Typography className="code">help 사용 가능한 명령어 보기</Typography>,
+        <Typography className="code">login 로그인하기</Typography>,
+        <Typography className="code">get_cele 축하받기</Typography>,
+        <Typography className="code">clear 명령 창 지우기</Typography>,
+        <Typography className="code" />,
+      ]);
+    }
+  }, []);
 
   return (
     <Box id="wrapper" className="centeralize">
@@ -26,7 +45,24 @@ export default function HomePage() {
               <Typography>Running: HPBD KDJ</Typography>
             </Box>
           </Box>
-          <Box id="codeContent">{lineData}</Box>
+          <Box id="codeContent">
+            {lineData}
+            <Box id="cursor">
+              $ <Box id="cursorBox" />
+            </Box>
+          </Box>
+          <Box id="selectors">
+            {choices?.map((choice) => (
+              <Button
+                className="choice"
+                onClick={() => {
+                  newLineWriter(choice);
+                }}
+              >
+                <Typography>{choice}</Typography>
+              </Button>
+            ))}
+          </Box>
         </Box>
       </Box>
     </Box>
